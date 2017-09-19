7fa6ced416354a1efa9dbd82405086c531114b1c
James Hayward <jhayward1980@gmail.com>
2017-09-19 14:52:28 +0100

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# config
+# Config
 A simple configuration library
+
+This library stores config in two files - a default file that you ship with your project with the configuration defaults, and a user-defined file that overrides and/or adds to these defaults. **Files will be created if they don't exist**
+
+
+## Usage
+
+First, create a config object and load the files:
+
+```
+$config = new Config();
+$config->load('/path/to/userConfig.json', '/path/to/defaultConfig.json');
+```
+
+Then you can get or set values into the config object.  Keys are dot-seperated.
+
+```
+$value = $config->getValue('key.name');
+$config->setValue('key.name', $value);
+```
+
+When you have finished, you need to call the save function to commit the config to disk.
+
+```
+$config->save();
+```
---
+++ src/Config.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace GeeksAreForLife\Config;
+
+use KHerGe\JSON\JSON;
+
+class Config
+{
+    private $config;
+    private $configFile;
+    private $default;
+
+    public function __construct()
+    {
+    }
+
+    public function load($configFile, $defaultFile)
+    {
+        $config = $this->readConfig($configFile);
+        $default = $this->readConfig($defaultFile);
+
+        $this->configFile = $configFile;
+
+        if ($default !== false) {
+            $this->default = $default;
+        }
+
+        if ($config !== false) {
+            $this->config = $config;
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function save()
+    {
+        $this->saveFile($this->config, $this->configFile);
+    }
+
+    public function getValue($key, $module = false)
+    {
+        $keys = $this->getKeys($key, $module);
+
+        $config = $this->config;
+        $default = $this->default;
+
+        foreach ($keys as $key) {
+            if (isset($config[$key])) {
+                $config = $config[$key];
+            } else {
+                $config = '';
+            }
+
+            if (isset($default[$key])) {
+                $default = $default[$key];
+            } else {
+                $default = '';
+            }
+        }
+
+        if ($config !== '') {
+            return $config;
+        } elseif ($default !== '') {
+            return $default;
+        } else {
+            return false;
+        }
+    }
+
+    public function setValue($key, $value, $module = false)
+    {
+        $keys = $this->getKeys($key, $module);
+
+        $config = &$this->config;
+        foreach ($keys as $key) {
+            if (!isset($config[$key])) {
+                $config[$key] = [];
+            }
+            $config = &$config[$key];
+        }
+        //var_dump($config);
+        if (empty($config) || !is_array($config)) {
+            $config = $value;
+        } else {
+            $config[] = $value;
+        }
+    }
+
+    private function getKeys($key, $module)
+    {
+        if ($module) {
+            $key = 'modules.'.strtolower($module).'.'.$key;
+        }
+
+        if (strpos($key, '.') !== false) {
+            $keys = explode('.', $key);
+        } else {
+            $keys = [$key];
+        }
+
+        return $keys;
+    }
+
+    private function readConfig($file)
+    {
+        if (file_exists($file)) {
+            $configJson = file_get_contents($file);
+
+            $json = new JSON();
+
+            try {
+                $json->lint($configJson);
+
+                return $json->decode($configJson, true);
+            } catch (\Exception $e) {
+                return false;
+            }
+        } else {
+            // create a blank file
+            $this->saveFile([], $file);
+
+            return [];
+        }
+    }
+
+    private function saveFile($content, $file)
+    {
+        $json = new JSON();
+
+        $json->encodeFile($content, $file, JSON_PRETTY_PRINT | JSON_NUMERIC_CHECK);
+    }
+}
